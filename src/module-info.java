89484a7c6d7238387c94bbd5963d0cae581c22b5
Martina De Maio <martinadem97@gmail.com>
2020-06-23 13:26:47 +0200

    modifiche

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,5 @@
 module deliverable2ISW {
-	requires org.eclipse.jgit;
-	requires java.logging;
-	requires java.json;
+	//requires org.eclipse.jgit;
+	//requires java.logging;
+	//requires java.json;
 }
